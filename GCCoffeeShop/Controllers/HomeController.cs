84ff1428d104e65988e2493847f5c924831e3d00
Anna B <akbaseley@gmail.com>
2019-02-21 16:08:25 -0500

    Breakout Start Point 2/21/2019

---
+++ GCCoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using GCCoffeeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GCCoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult SaveUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.User = user;
+                return View();
+            }
+            else
+            {
+                return View("Registration");
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ GCCoffeeShop/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GCCoffeeShop.Models
+{
+    public class User
+    {
+        [Required]
+        [RegularExpression(@"^[A-Z{1}]+[a-zA-z{1,30}]+$", ErrorMessage = "Please enter a valid name.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z{1}]+[a-zA-z{1,30}]+$", ErrorMessage = "Please enter a valid name.")]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9{5,30}]+@[a-zA-A0-9{5,10}]+\.[a-zA-Z0-9{2,3}]+$", ErrorMessage = "Incorrect E-mail Format!")]
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Please enter a valid number!")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+
+        public User(string firstName, string lastName, string email, int age, string phoneNumber, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Age = age;
+            PhoneNumber = phoneNumber;
+            Password = password;
+        }
+
+
+        public User() { }
+    }
+}
